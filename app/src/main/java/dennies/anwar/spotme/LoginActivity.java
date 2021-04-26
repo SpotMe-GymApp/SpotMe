dbb3d6f6b1f04ab1d7453c8ed498b8fa0750c2ec
Dennies Anwar <dennies.anwar34@myhunter.cuny.edu>
2021-04-26 19:21:37 -0400

    Login Screen works

--- app/src/main/java/dennies/anwar/spotme/LoginActivity.java
+++ app/src/main/java/dennies/anwar/spotme/LoginActivity.java
@@ -2,11 +2,16 @@ package dennies.anwar.spotme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,11 +39,33 @@ public class LoginActivity extends AppCompatActivity {
                 
             }
 
-            private void loginUser(String username, String password) {
-                Log.i(TAG, "Attempting to login user " + username);
-            }
+
         });
         
         
     }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                goMainActivity();
+            }
+
+            // Navigate to the main activity
+            
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+
 }
