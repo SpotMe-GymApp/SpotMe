bae260ea091f948b3b1fe0093bff5cc19ded5157
Dennies Anwar <dennies.anwar34@myhunter.cuny.edu>
2021-04-28 11:27:17 -0400

    Adding Edit Profile Screen

--- app/src/main/java/dennies/anwar/spotme/EditProfileActivity.java
+++ app/src/main/java/dennies/anwar/spotme/EditProfileActivity.java
@@ -3,12 +3,26 @@ package dennies.anwar.spotme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    public static final String TAG = "EditProfileActivity";
+    private EditText etName;
+    private EditText etAge;
+    private EditText etWeight;
+    private EditText etBio;
+    private EditText etBench;
+    private EditText etSquat;
+    private EditText etDeadlift;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        etName = findViewById(R.id.etName);
+        etAge = findViewById(R.id.etAge);
+        et
     }
 }
